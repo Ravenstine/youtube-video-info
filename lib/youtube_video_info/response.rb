341d4d45a4958d2231dcff4242ed18c494b9622d
benjamin <benjamin@benjamin>
2015-08-24 21:04:36 -0700

    Add headers conveniece method to response.

--- lib/youtube_video_info/response.rb
+++ lib/youtube_video_info/response.rb
@@ -24,5 +24,20 @@ module YoutubeVideoInfo
       end
       parsed_links
     end
+    def video_download_headers
+      # This is just for convenience.
+      # Many of these are here to make the request appear genuine.
+      {
+        "Connection" => "keep-alive",
+        "Origin" => "https://www.youtube.com",
+        "X-FirePHP-Version" => "0.0.6",
+        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.95 Safari/537.36",
+        "Accept" => "*/*",
+        "DNT" => "1",
+        "Referer" => "https://www.youtube.com/watch?v=#{video_id[0]}",
+        "Accept-Encoding" => "gzip, deflate, sdch",
+        "Accept-Language" => "en-US,en;q=0.8,de-DE;q=0.6,de;q=0.4"
+      }
+    end
   end
 end
